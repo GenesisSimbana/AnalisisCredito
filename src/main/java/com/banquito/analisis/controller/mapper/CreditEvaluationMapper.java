1588f9ce9ef6fc72d9647a6c9992f44f6bbf1d3f
nsvillegas@espe.edu.ec <nsvillegas@espe.edu.ec>
2025-07-11 18:44:32 -0500

    configuracion

--- src/main/java/com/banquito/analisis/controller/mapper/CreditEvaluationMapper.java
+++ src/main/java/com/banquito/analisis/controller/mapper/CreditEvaluationMapper.java
@@ -4,7 +4,6 @@ import com.banquito.analisis.controller.dto.CreditEvaluationDTO;
 import com.banquito.analisis.model.EvaluacionCrediticia;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CreditEvaluationMapper {
